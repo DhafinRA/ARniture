19a2715f7ea48f8ab35e4f520c70e791c5b3d6ad
Wahyu AR <wahyuariefrahman2@gmail.com>
2024-12-05 21:03:47 +0700

    update menu (sudah bisa ambil product + detail. image tinggal taruh di image lalu edit script), tambah global data static (bisa akses di scene mana aja untuk perkondisian object ar mode)

--- ARniture/Assets/Script/menu/MenuSetting.cs
+++ ARniture/Assets/Script/menu/MenuSetting.cs
@@ -24,13 +24,14 @@ public class MenuSetting : MonoBehaviour
     [SerializeField] TMP_Text det_cat;
 
     [Header("Content")]
-    public string[] kategori;
+    [SerializeField] string[] kategori;
     [TextArea]
-    public string[] deskripsi;
-    public string[] product;
+    [SerializeField] string[] deskripsi;
+    [SerializeField] string[] product;
     [TextArea]
-    public string[] product_desc;
-    string namaBarang;
+    [SerializeField] string[] product_desc;
+    [SerializeField] Sprite[] product_img;
+
 
     Vector2 startpos;
     Vector2 endpos;
@@ -38,13 +39,15 @@ public class MenuSetting : MonoBehaviour
     float movespd = 10.0f;
     bool ismoving=false;
 
+    TextAsset txtprod;
+    TextAsset txtdet;
+
     void Start()
     {
         bg.color = Color.white;
         MenuList[0].SetActive(true);
         MenuList[1].SetActive(false);
         MenuList[2].SetActive(false);
-        MenuList[3].SetActive(false);
 
         startpos = panelmenu.anchoredPosition;
         endpos = startpos;
@@ -74,77 +77,35 @@ public class MenuSetting : MonoBehaviour
     }
     void productChange()
     {
-        if(cat_kat.text == kategori[0])
+        //prod
+        txtprod = Resources.Load<TextAsset>(cat_kat.text + "/Product");
+        if (txtprod != null)
         {
-            product[0] = "Sofa";
-            product[1] = "Meja";
-            product[2] = "Rak Tv";
-            product[3] = "Karpet";
-            product[4] = "Lemari Hias";
-            product[5] = "Kursi";
-
-            product_desc[0] = "Perabot duduk dengan bantalan empuk, biasanya dilapisi kain atau kulit, yang dirancang untuk kenyamanan di ruang tamu atau ruang keluarga. Sofa tersedia dalam berbagai ukuran dan gaya, seperti sofa dua tempat duduk, sectional, atau sofa sudut.";
-            product_desc[1] = "Furnitur dengan permukaan datar yang berfungsi untuk bekerja, makan, atau meletakkan benda. Meja bisa berbentuk persegi, bulat, atau persegi panjang, dan sering dilengkapi dengan kaki penyangga dari kayu, logam, atau bahan lainnya.";
-            product_desc[2] = "Furnitur rendah yang dirancang untuk menopang televisi dan perangkat hiburan lainnya, seperti konsol game, speaker, atau pemutar DVD. Rak TV biasanya memiliki ruang penyimpanan tambahan berupa laci atau rak terbuka.";
-            product_desc[3] = "Penutup lantai yang terbuat dari kain tenun atau bahan sintetis, berfungsi untuk menambah kenyamanan dan estetika ruangan. Karpet tersedia dalam berbagai motif, warna, dan ukuran untuk disesuaikan dengan dekorasi ruangan.";
-            product_desc[4] = "Lemari dengan desain terbuka atau tertutup yang digunakan untuk memajang barang dekoratif, koleksi, atau barang berharga. Biasanya dilengkapi dengan rak kaca atau kayu dan terkadang memiliki pencahayaan interior untuk menonjolkan isinya.";
-            product_desc[5] = "Furnitur untuk duduk dengan desain yang bervariasi, mulai dari kursi makan, kursi kerja, hingga kursi santai. Kursi dapat terbuat dari berbagai bahan, seperti kayu, logam, atau plastik, dengan atau tanpa bantalan.";
-        }
-        else if (cat_kat.text == kategori[1])
-        {
-            product[0] = "Meja Makan";
-            product[1] = "Kursi Makan";
-            product[2] = "Rak Penyimpanan";
-            product[3] = "Taplak Meja";
-            product[4] = "Piring";
-            product[5] = "Gelas";
-
-            product_desc[0] = kategori[1] + "1";
-            product_desc[1] = kategori[1] + "2";
-            product_desc[2] = kategori[1] + "3";
-            product_desc[3] = kategori[1] + "4";
-            product_desc[4] = kategori[1] + "5";
-            product_desc[5] = kategori[1] + "6";
-        }
-        else if (cat_kat.text == kategori[2])
-        {
-            product[0] = "Kasur";
-            product[1] = "Lemari";
-            product[2] = "Nakas";
-            product[3] = "Meja Belajar";
-            product[4] = "Rak";
-            product[5] = "Lampu Belajar";
-
-            product_desc[0] = kategori[2] + "1";
-            product_desc[1] = kategori[2] + "2";
-            product_desc[2] = kategori[2] + "3";
-            product_desc[3] = kategori[2] + "4";
-            product_desc[4] = kategori[2] + "5";
-            product_desc[5] = kategori[2] + "6";
-        }
-        else if (cat_kat.text == kategori[3])
-        {
-            product[0] = "Kabinet Dapur";
-            product[1] = "Meja Dapur";
-            product[2] = "Rak Piring";
-            product[3] = "Rak Gantung";
-            product[4] = "Rak Anggur";
-            product[5] = "Lemari Penyimpanan";
-
-            product_desc[0] = kategori[3] + "1";
-            product_desc[1] = kategori[3] + "2";
-            product_desc[2] = kategori[3] + "3";
-            product_desc[3] = kategori[3] + "4";
-            product_desc[4] = kategori[3] + "5";
-            product_desc[5] = kategori[3] + "6";
+            product = txtprod.text.Split('\n');
         }
         else
         {
-            for (int i = 0; i < product.Length; i++)
-            {
-                product[i] = i.ToString();
-                product_desc[i] = "desc"+i.ToString();
-            }
+            Debug.LogError("File tidak ditemukan di folder Resources!");
+        }
+        //prod_det
+        txtdet = Resources.Load<TextAsset>(cat_kat.text + "/Product_Detail");
+        if (txtdet != null)
+        {
+            product_desc = txtdet.text.Split('\n');
+        }
+        else
+        {
+            Debug.LogError("File tidak ditemukan di folder Resources!");
+        }
+        //prod_img
+        product_img = Resources.LoadAll<Sprite>(cat_kat.text + "/Image");
+        if (product_img.Length > 0)
+        {
+            Debug.Log("Success Load Sprite");
+        }
+        else
+        {
+            Debug.LogError("Sprite Tidak Ditemukan!");
         }
     }
 
@@ -172,7 +133,7 @@ public class MenuSetting : MonoBehaviour
             cat_product[i].text = product[i];
         }
     }
-    public void rMakanClick()
+    public void rKerjaClick()
     {
         catClick();
         cat_kat.text = kategori[1];
@@ -237,10 +198,12 @@ public class MenuSetting : MonoBehaviour
                 bg.color = Color.yellow; break;
             default: bg.color = Color.gray; break;
         }
+        //det_img.sprite = product_img[a];
         det_name.text = cat_product[a].text;
         det_desc.text = product_desc[a];
         det_cat.text = "Ruang "+ cat_kat.text;
-        namaBarang = cat_product[a].text;
+        DataGlobal.Kategori = cat_kat.text;
+        DataGlobal.Produk = det_name.text;
     }
     public void detBackClick()
     {
@@ -261,34 +224,6 @@ public class MenuSetting : MonoBehaviour
         ismoving = true;
     }
 
-    public void ARmodeClick()
-    {
-        bg.enabled = false;
-        MenuList[2].SetActive(false);
-        MenuList[3].SetActive(true);
-    }
-
-    public void ARmodeBack()
-    {
-        bg.enabled = true;
-        // MenuList[0].SetActive(true);
-        MenuList[2].SetActive(true);
-        MenuList[3].SetActive(false);
-    }
-
-    public void ObjectDetection()
-    {
-        ARmodeClick();
-        for (int i = 0; i < product.Length; i++)
-        {
-            if (namaBarang.Equals(product[i]))
-            {
-                Debug.Log(namaBarang);
-                break;
-            }   
-        }
-    }
-
     public void exit()
     {
 #if UNITY_EDITOR
@@ -296,5 +231,11 @@ public class MenuSetting : MonoBehaviour
 #else
             Application.Quit(); // Menutup aplikasi jika sudah di build
 #endif
+    }
+
+    public static class DataGlobal
+    {
+        public static string Kategori;
+        public static string Produk;
     }
 }
